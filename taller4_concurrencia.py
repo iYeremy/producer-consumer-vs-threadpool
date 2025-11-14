ab73bb2213137787fe5a00f441486d1676741c5b
Yeremy <yeremyvegap@gmail.com>
2025-11-14 01:36:32 -0500

    feat: funcion productor con inicio, procesamiento y finalizacion en mensajes

--- taller4_concurrencia.py
+++ taller4_concurrencia.py
@@ -2,8 +2,16 @@ import threading, queue, time, random
 
 cola = queue.Queue(maxsize=10)
 
-def productor():
-    pass
+def productor(nombre):
+    print(f"Iniciando productor [{nombre}]...")
+    for i in range(5):
+        item = f"[{nombre}] - Helado - {i}"
+        cola.put(item) # bloquea si esta llena la cola
+        print(f"Productor [{nombre}] hizo un {item}")
+        time.sleep(random.uniform(0.2, 0.6))
+
+    cola.put(None) # senial para los consumidores 
+    print(f"{nombre} finalizo produccion")
 
 def consumidor():
     pass

e6393b33fcf0e45a438e24803c189d5476b0c1a3
Yeremy <yeremyvegap@gmail.com>
2025-11-14 22:03:37 -0500

    fix: arreglar tiempos repetidos de ejecucion en consola

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # main.py
-import time
 from src.controller.controller import Controller
 
 def main():
-    inicio = time.time()
     app = Controller()
     app.run()
-    final = time.time()
-    print("Tiempo total:", final - inicio)
 
 if __name__ == "__main__":
     main()

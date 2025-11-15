a997bd83220b402a1eee04848193e852665c11ed
Yeremy <yeremyvegap@gmail.com>
2025-11-14 22:02:16 -0500

    feat: calcular productos hechos y producidos

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -2,39 +2,51 @@ import threading
 from src.models.producer import Producer
 from src.models.consumer import Consumer
 from src.view.console import ConsoleView
+import time
 
 class Controller:
     def run(self):
+
+        producciones = []
+        consumos = []
+
+        inicio = time.time()
         threads = []
 
-        # productores
+        # PRODUCTORES
         for i in range(2):
-            def producer_job(i=i):
+            def producer_job(i=i):  
                 ConsoleView.show_producer_start(i)
                 p = Producer(i)
                 items = p.produce()
+                producciones.extend(items)
                 ConsoleView.show_produced(i, items)
                 ConsoleView.show_producer_end(i)
 
             threads.append(threading.Thread(target=producer_job))
 
-        # consumidores
+        # CONSUMIDORES
         for i in range(3):
             def consumer_job(i=i):
                 ConsoleView.show_consumer_start(i)
                 c = Consumer(i)
                 items = c.consume()
+                consumos.extend(items)
                 ConsoleView.show_consumed(i, items)
                 ConsoleView.show_consumer_end(i)
 
             threads.append(threading.Thread(target=consumer_job))
 
-        # iniciar hilos
+        # Ejecutar hilos
         for h in threads:
             h.start()
 
-        # esperar hilos
         for h in threads:
             h.join()
 
-        print("Procesamiento completado")
+        fin = time.time()
+
+        print("\n===== RESULTADOS =====")
+        print("Tareas producidas:", len(producciones))
+        print("Tareas consumidas:", len(consumos))
+        print("Tiempo total:", round(fin - inicio, 3), "segundos")
